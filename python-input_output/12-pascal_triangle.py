4247ac76d21b54b949e51dedf92d319573692bcb
danish872 <danishimwe872@gmail.com>
2025-06-03 12:51:58 +0200

    commit of python input-output's files

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module for pascal_triangle method"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers
+    representing Pascal's triangle of n."""
+    if n <= 0:
+        return []
+
+    def new_row(prev_row):
+        row = [1]
+        for i in range(1, len(prev_row)):
+            row.append(prev_row[i - 1] + prev_row[i])
+        row.append(1)
+        return row
+
+    triangle = [[1]]
+    for i in range(1, n):
+        triangle.append(new_row(triangle[i - 1]))
+
+    return triangle
